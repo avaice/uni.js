963538644efc0b59a276691e082b2f2710cf4611
あいす <80768507+avaice@users.noreply.github.com>
2022-01-06 01:23:43 +0900

    Add files via upload

---
+++ uni.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+    let dom = document.querySelectorAll('p');
+    for(let j = 0; j < dom.length; j++){
+        
+        const element = dom[j];
+        console.log(element.innerHTML.length)
+        const n = Math.floor(element.innerHTML.length / 2);
+        const mark = (element.innerHTML.length % 2 == 1 ? "✹" : "") ;
+        element.innerHTML = "";
+        for(let i = 0; i < n; i++){
+            element.innerHTML = element.innerHTML + "うに";
+        }
+        element.innerHTML = element.innerHTML + mark;
+        ;
+
+    }
+    
+}
+
